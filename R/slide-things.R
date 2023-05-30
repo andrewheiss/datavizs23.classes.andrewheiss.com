458797d1085f553a8f27bdb8026230ddff8ab44a
Andrew Heiss <andrew@andrewheiss.com>
2023-05-29 22:41:32 -0400

    Center slide buttons

--- R/slide-things.R
+++ R/slide-things.R
@@ -1,3 +1,3 @@
 slide_buttons <- function(slide_id) {
-  glue::glue('<p class="buttons"><a class="btn btn-danger" target="_blank" href="{slide_id}.html"><i class="fa-solid fa-arrow-up-right-from-square"></i> View all slides in new window</a> <a class="btn btn-danger" target="_blank" href="{slide_id}.pdf" role="button"><i class="fa-solid fa-file-pdf"></i> Download PDF of all slides</a></p>')
+  glue::glue('<p class="text-center buttons"><a class="btn btn-danger" target="_blank" href="{slide_id}.html"><i class="fa-solid fa-arrow-up-right-from-square"></i> View all slides in new window</a> <a class="btn btn-danger" target="_blank" href="{slide_id}.pdf" role="button"><i class="fa-solid fa-file-pdf"></i> Download PDF of all slides</a></p>')
 }
